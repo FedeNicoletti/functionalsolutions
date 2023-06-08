3dea083a8eeb01e68149895bfd81948dfac12443
FedeNicoletti <federiconicoletti@outlook.com>
2023-06-08 16:30:39 -0300

    update blog

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./blog.css";
 import Article from "../../components/article/Article";
 import { santiago, valentina, nicolas, ezequiel, franco } from "./imports";
@@ -57,8 +57,61 @@ export default function Blog() {
     }
   };
 
+  useEffect(() => {
+    const handleResize = () => {
+      const screenWidth = window.innerWidth;
+      if (screenWidth <= 700) {
+        // Restaurar el orden original
+        setGroupA([
+          {
+            id: "santiago",
+            imgUrl: santiago,
+            name: "Santiago Robetto",
+            title:
+              "Licenciado en Kinesiología\nCertificación FMS nivel 1 y 2.\nTécnicas osteopaticas.\nFormación en Diagnóstico y Terapia Mecánica (Método Mckenzie).\nPunción seca nivel 1 y 2.\nFormación en concepto Mulligan.",
+          },
+        ]);
+        setGroupB([
+          {
+            id: "valentina",
+            imgUrl: valentina,
+            name: "Valentina Robetto",
+            title:
+              "Certificación internacional en antropometría, nivel 1 S.A.K.\nExperiencia en nutrición deportiva.",
+          },
+          {
+            id: "nicolas",
+            imgUrl: nicolas,
+            name: "Nicolás Aguilera",
+            title:
+              "Especialista en psicoterapia cognitiva.\nConocimientos en psoicología deportiva.\nTrabajo interdisciplinario en deportistas de alto rendimiento.\nAbordaje online con profesionales y deportistas en España y Argentina.",
+          },
+          {
+            id: "ezequiel",
+            imgUrl: ezequiel,
+            name: "Ezequiel Martín",
+            title:
+              "Licenciado en Nutrición\nCertificación en nutrición deportiva del Club Barcelona.\nExperto en tratamiento de obesidad.\nDiplomado en preparación deportiva.\nExperto en deportes de fuerza y resistencia.",
+          },
+          {
+            id: "franco",
+            imgUrl: franco,
+            name: "Franco Montes",
+            title:
+              "Licenciado en Kinesiología\nCertificación FMS nivel 1 y 2.\nFormación en concepto Mulligan.\nTécnicas osteopaticas.\nPunción seca nivel 1 y 2.",
+          },
+        ]);
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
-    <div className="fs__blog section__padding" id="profesionales">
+    <div className="fs__blog section__padding" id="blog">
       <div className="fs__blog-heading">
         <h1 className="gradient__text">Nuestros profesionales</h1>
       </div>
